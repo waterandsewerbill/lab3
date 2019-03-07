7e3b6c8e0a247d36b7d555ffa7e8b8a7a213fcda
waterandsewerbill <rbrooks1066@gmail.com>
2019-03-07 13:46:22 -0500

    March 7

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,21 +1,42 @@
 package com.example.lab3;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import static com.example.lab3.R.id.r2EditText;
+import static com.example.lab3.R.layout.*;
 import static com.example.lab3.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button1;
+    //private Button button1;
+    private static final String SHARED_PREFS = "sharedPrefs";
+    private SharedPreferences prefs;
+    private EditText textField;
+    private static final String TEXT = "text";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(activity_main);
 
-        button1 = (Button)findViewById(R.id.r5Button);
+        textField = (EditText)findViewById(r2EditText);
+
+        prefs = getSharedPreferences("DefaultEmail", Context.MODE_PRIVATE);
+
+        String emailString = prefs.getString("DefaultEmail", "r@b.com");
+
+        textField.setText(emailString);
+
+        Button button1 = (Button)findViewById(R.id.r5Button);
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,12 +44,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        loadData();
     }
+
     public void openActivity2(){
         Intent intent = new Intent(this, ProfileActivity.class);
+        intent.putExtra(SHARED_PREFS, textField.getText().toString());
         startActivity(intent);
 
     }
+    private void saveData(){
+        String saveData = textField.getText().toString();
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(TEXT, saveData);
+        editor.apply();
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
 
+    }
+    private void loadData() {
+        String savedString = prefs.getString(TEXT, "");
+        textField.setText(savedString);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        saveData();
+    }
 }
--- app/src/main/java/com/example/lab3/ProfileActivity.java
+++ app/src/main/java/com/example/lab3/ProfileActivity.java
@@ -5,33 +5,45 @@ import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.util.Log;
 
 
 public class ProfileActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    private ImageButton mImageButton;
-    
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+    ImageButton takePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mImageButton = (ImageButton) findViewById(R.id.ap_r7Button);
-    }
+        setContentView(R.layout.profile_activity);
+        Log.e(ACTIVITY_NAME, "In function:" + "onCreate");
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+        takePicture = (ImageButton)findViewById(R.id.ap_r7Button);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageButton.setImageBitmap(imageBitmap);
-        }
+        takePicture.setOnClickListener( c -> {
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+
+
+
+
+        Button button1 = (Button)findViewById(R.id.r5Button);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity1();
+            }
+        });
     }
 
     private void dispatchTakePictureIntent() {
@@ -41,8 +53,50 @@ public class ProfileActivity extends AppCompatActivity {
         }
     }
 
+    private void openActivity1(){
+        Intent intent = new Intent(this, ChatRoomActivity.class);
+        //intent.putExtra(SHARED_PREFS, textField.getText().toString());
+        startActivity(intent);
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e(ACTIVITY_NAME, "In function:" + "onActivityResult");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            takePicture.setImageBitmap(imageBitmap);
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "In function:" + "onStart");
 
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In function:" + "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In function:" + "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In function:" + "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In function:" + "onResume");
+    }
 
 }
